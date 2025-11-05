cd4dd8f47d8bbb8bf5b0d704cd20dad93fcf9f34
orly07 <delacruzorlando776@gmail.com>
2025-11-06 00:11:59 +0800

    add: img characters for the knght's tale

--- src/data/data.js
+++ src/data/data.js
@@ -28,6 +28,20 @@ import ApothecaryImg from "../assets/stories/romeo-and-juliet/characters/The Apo
 
 // The Knight Tale
 import TheKnightTaleCover from "../assets/stories/the-knights-tale/the-knight-tale-cover.jpg";
+import TheseusImg from "../assets/stories/the-knights-tale/theseus.jpg";
+import HippolytaImg from "../assets/stories/the-knights-tale/hippolyta.jpg";
+import EmelyeImg from "../assets/stories/the-knights-tale/emelye.jpg";
+import PalamonImg from "../assets/stories/the-knights-tale/palamon.jpg";
+import ArciteImg from "../assets/stories/the-knights-tale/arcite.jpg";
+import CreonImg from "../assets/stories/the-knights-tale/creo.jpg";
+import PerotheusImg from "../assets/stories/the-knights-tale/perotheus.jpg";
+import EgeusImg from "../assets/stories/the-knights-tale/egeus.jpg";
+import VenusImg from "../assets/stories/the-knights-tale/venus.jpg";
+import MarsImg from "../assets/stories/the-knights-tale/mars.jpg";
+import DianaImg from "../assets/stories/the-knights-tale/diana.jpg";
+import SaturnImg from "../assets/stories/the-knights-tale/saturn.jpg";
+import JailerImg from "../assets/stories/the-knights-tale/jailer.jpg";
+import ServantsImg from "../assets/stories/the-knights-tale/the-servants.jpg";
 
 // STORIES
 export const storiesData = [
@@ -163,6 +177,7 @@ export const storiesData = [
     ],
   },
 
+  // The Knight's Tale
   {
     id: "the-knights-tale",
     title: "The Knight's Tale",
@@ -173,6 +188,106 @@ export const storiesData = [
     video: "https://www.youtube.com/embed/f58u4bDlmnw?si=1zaUJ_-ZRhJkdFyU",
     fullStories:
       "https://www.owleyes.org/text/canterbury-tales/read/the-knights-tale#root-218796-3",
+    characters: [
+      {
+        name: "Theseus",
+        img: TheseusImg,
+        lines: "",
+        description:
+          "He is the Duke of Athens, a noble and wise ruler who values order and justice. After winning a war against Thebes, he shows mercy and fairness. Theseus represents the ideal leader in both love and law, balancing power with compassion.",
+      },
+      {
+        name: "Hippolyta",
+        img: HippolytaImg,
+        lines: "",
+        description:
+          "She is the Queen of the Amazons and wife of Theseus. Once a warrior queen, she now symbolizes peace and unity, showing how strength can exist alongside grace and harmony.",
+      },
+      {
+        name: "Emelye (Emily)",
+        img: EmelyeImg,
+        lines: "",
+        description:
+          "She is the younger sister of Hippolyta and the woman both Palamon and Arcite fall in love with. Emelye is beautiful, kind, and pure, representing the image of ideal womanhood in medieval courtly love.",
+      },
+      {
+        name: "Palamon",
+        img: PalamonImg,
+        lines: "",
+        description:
+          "A noble Theban knight who is captured and imprisoned by Theseus. While in prison, he sees Emelye and immediately falls in love with her. His love is sincere and faithful, showing devotion and loyalty above all.",
+      },
+      {
+        name: "Arcite",
+        img: ArciteImg,
+        lines: "",
+        description:
+          "Palamon’s cousin and fellow prisoner. He also falls in love with Emelye, but his love is more passionate and ambitious. Arcite’s actions reveal the struggle between desire and honor.",
+      },
+      {
+        name: "Creon",
+        img: CreonImg,
+        lines: "",
+        description:
+          "The cruel king of Thebes who disrespects the dead and rules with tyranny. His defeat by Theseus begins the events of the story. Creon represents pride, injustice, and the downfall of corrupt rulers.",
+      },
+      {
+        name: "Perotheus",
+        img: PerotheusImg,
+        lines: "",
+        description:
+          "A loyal friend of both Theseus and Arcite. His friendship with Arcite helps to free him from prison. Perotheus stands for loyalty and the value of true friendship.",
+      },
+      {
+        name: "Egeus",
+        img: EgeusImg,
+        lines: "",
+        description:
+          "Theseus’s old and wise father. He offers guidance and reminds Theseus about destiny and how people must accept the will of the gods.",
+      },
+      {
+        name: "Venus",
+        img: VenusImg,
+        lines: "",
+        description:
+          "The goddess of love. Palamon prays to her for success in love, showing his pure emotional devotion. She symbolizes love’s gentleness and constancy.",
+      },
+      {
+        name: "Mars",
+        img: MarsImg,
+        lines: "",
+        description:
+          "The god of war. Arcite prays to him for victory in battle, showing his fiery passion and ambition. Mars symbolizes strength, conflict, and desire.",
+      },
+      {
+        name: "Diana",
+        img: DianaImg,
+        lines: "",
+        description:
+          "The goddess of chastity and the hunt. Emelye prays to her because she wishes to remain unmarried and pure. Diana represents independence and the freedom of choice.",
+      },
+      {
+        name: "Saturn",
+        img: SaturnImg,
+        lines: "",
+        description:
+          "The god of fate and time who has the final say in the outcome of the story. He shows that destiny controls human life no matter what mortals plan.",
+      },
+      {
+        name: "Jailer (or Gaoler)",
+        img: JailerImg,
+        lines: "",
+        description:
+          "The man who watches over Palamon and Arcite while they are imprisoned. He represents the control of society and the limitations placed on individuals by authority.",
+      },
+      {
+        name: "The Servants and Attendants",
+        img: ServantsImg,
+        lines: "",
+        description:
+          "Unnamed figures who serve Theseus and others. They add to the courtly setting and help show the order and discipline of noble life in Athens.",
+      },
+    ],
   },
 ];
 
@@ -180,7 +295,7 @@ export const storiesData = [
 import Sonnet18Cover from "../assets/sonnets/Sonnet18.png";
 import Sonnet29Cover from "../assets/sonnets/Sonnet29.png";
 import Sonnet73Cover from "../assets/sonnets/Sonnet73.png";
-import Soonet55Cover from "../assets/sonnets/Sonnet55.png"
+import Soonet55Cover from "../assets/sonnets/Sonnet55.png";
 
 export const sonnetsData = [
   {
