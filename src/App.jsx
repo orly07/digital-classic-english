0aad80bf027eeb00d344216e9d94f27307ce7af0
orly07 <delacruzorlando776@gmail.com>
2025-11-06 03:50:16 +0800

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+// App.js
 import React, { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import "./styles/global.css";
-import "./components/ErrorBoundary/ErrorBoundary";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import "./styles/global.css";
 
 // Lazy load pages
 const LandingPage = lazy(() => import("./pages/LandingPage/LandingPage"));
@@ -11,16 +11,13 @@ const Story = lazy(() => import("./pages/Story/Story"));
 const About = lazy(() => import("./modules/About/About"));
 const SonnetDetail = lazy(() => import("./pages/SonnetDetail/SonnetDetail"));
 
-// Loading component
 const LoadingFallback = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "50vh",
-    }}
-  >
+  <div style={{ 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '50vh' 
+  }}>
     <div>Loading...</div>
   </div>
 );
@@ -35,6 +32,8 @@ function App() {
             <Route path="/story/:id" element={<Story />} />
             <Route path="/sonnet/:id" element={<SonnetDetail />} />
             <Route path="/about" element={<About />} />
+            {/* Catch all route - redirect to home */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Suspense>
       </Layout>
